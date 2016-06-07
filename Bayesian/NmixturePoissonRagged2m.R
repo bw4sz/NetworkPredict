eef1534771c09a905259573a2b911e23634b452e
bw4sz <benweinstein2010@gmail.com>
2016-06-06 21:48:33 -0700

    model runs, but needs to start with two parameters, resources are still 0

--- Bayesian/NmixturePoissonRagged2m.R
+++ Bayesian/NmixturePoissonRagged2m.R
@@ -3,31 +3,31 @@ sink("Bayesian/NmixturePoissonRagged2m.jags")
 
 cat("
     model {
-    #Compute intensity for each pair of birds and plants
+    #Compute true state for each pair of birds and plants
     for (i in 1:Birds){
     for (j in 1:Plants){
-    for (k in 1:Cameras){
+    for (k in 1:Times){
     
     #Process Model
-    logit(rho[i,j,k])<-alpha[i] + beta1[i] * Traitmatch[i,j] + beta2[i] * resources[j,k]
+    logit(rho[i,j,k])<-alpha[i] + beta1[i] * Traitmatch[i,j] + beta2[i] * resources[i,j,k]
     
-    #True number of interactions
+    #True State
     S[i,j,k] ~ dbern(rho[i,j,k])
     }
     }
     }
-
-    #Observed counts for each day of sampling
+    
+    #Observation Model
     for (x in 1:Nobs){
     
     #Observation Process for cameras
-    detect_cam[x]<-dcam[Bird[x]] * cam_surveys[x]
+    detect_cam[x]<-dcam * cam_surveys[x]
 
     #Observation Process for transects
-    detect_transect[x]<-dtrans[Bird[x]] * trans_surveys[x]
+    detect_transect[x]<-dtrans * trans_surveys[x]
 
-    Yobs_camera[x] ~ dbin(detect_cam[x],S[Bird[x],Plant[x],Time[x]])    
-    Yobs_transect[x] ~ dbin(detect_transect[x],S[Bird[x],Plant[x],Time[x]])    
+    Yobs_camera[x] ~ dbern(detect_cam[x] * S[Bird[x],Plant[x],Time[x]])    
+    Yobs_transect[x] ~ dbern(detect_transect[x] * S[Bird[x],Plant[x],Time[x]])    
 
     #Assess Model Fit
 
@@ -40,48 +40,46 @@ cat("
     
     }
 
-    #Species level priors
-    
-    for (i in 1:Birds){
+    #Transect Prior
     #Detect priors, logit transformed
-
     #For Cameras
-    logit(dcam[i]) <- dcam_logit[i]
-    dcam_logit[i] ~ dnorm(dprior_cam,tau_dcam)
-
+    logit(dcam) <- dcam_logit
+    dcam_logit ~ dnorm(0,0.386)
+    
     #For Transects
-    logit(dtrans[i]) <- dtrans_logit[i]
-    dtrans_logit[i] ~ dnorm(dprior_trans,tau_dtrans)
-
-    alpha[i] ~ dnorm(intercept,tau_alpha)
-    beta1[i] ~ dnorm(gamma1,tau_beta1)    
-    beta2[i] ~ dnorm(gamma2,tau_beta2)    
+    logit(dtrans) <- dtrans_logit
+    dtrans_logit ~ dnorm(0,0.386)
+    
+    #Species level priors
+    for (i in 1:Birds){
+      alpha[i] ~ dnorm(intercept,tau_alpha)
+      beta1[i] ~ dnorm(gamma1,tau_beta1)    
+      beta2[i] ~ dnorm(gamma2,tau_beta2)    
     }
 
     #Hyperpriors
     #Slope grouping
     gamma1~dnorm(0,0.0001)
     gamma2~dnorm(0,0.0001)
-    gamma3~dnorm(0,0.0001)
     
     #Intercept grouping
     intercept~dnorm(0,0.0001)
     
     #Detection group prior
-    dprior_cam ~ dnorm(0,0.5)
-    dprior_trans ~ dnorm(0,0.5)
+    #dprior_cam ~ dnorm(0,0.386)
+    #dprior_trans ~ dnorm(0,0.386)
 
     # Group intercept variance
     tau_alpha ~ dt(0,1,1)I(0,1)
     sigma_alpha<-pow(1/tau_alpha,2) 
     
     #Group effect detect camera
-    tau_dcam ~ dunif(0,10)
-    sigma_dcam<-pow(1/tau_dcam,.5)
+    #tau_dcam ~ dunif(0,10)
+    #sigma_dcam<-pow(1/tau_dcam,.5)
     
     #Group effect detect camera
-    tau_dtrans ~ dunif(0,10)
-    sigma_dtrans<-pow(1/tau_dtrans,.5)
+    #tau_dtrans ~ dunif(0,10)
+    #sigma_dtrans<-pow(1/tau_dtrans,.5)
 
     #Group Effect of traits
     tau_beta1 ~ dt(0,1,1)I(0,)
