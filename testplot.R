28ce7bbcbf35c44d1a2b2d72d1af315e54d3e03e
bw4sz <benweinstein2010@gmail.com>
2016-07-06 08:50:18 -0700

    network predict

--- testplot.R
+++ testplot.R
@@ -1,5 +1,5 @@
-predy<-trajF(alpha=-1.45,beta1=-0.056,trait=indat$Traitmatch,resources=scale(indat$FlowerA),beta2=0.22,beta3=-0.01)
-predy2<-trajF(alpha=-1.45,beta1=-0.056,trait=indat$Traitmatch,resources=scale(indat$FlowerA),beta2=0.22,beta3=-.1)
+predy<-trajF(alpha=-1.43,beta1=-0.056,trait=indat$Traitmatch,resources=scale(indat$All_Flowers),beta2=-0.11,beta3=0.001)
+predy2<-trajF(alpha=-1.43,beta1=-0.056,trait=indat$Traitmatch,resources=scale(indat$All_Flowers),beta2=0,beta3=0.001)
 
 ggplot(data=predy,aes(x=trait)) + geom_ribbon(aes(ymin=lower,ymax=upper),alpha=0.4,fill="red")  +  theme_bw() + ylab("Interactions") + xlab("Difference between Bill and Corolla Length")  + geom_line(aes(y=mean)) + geom_line(data=predy2,aes(x=trait,y=mean),col="blue")
 
